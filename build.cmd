f2d9c20b2022896a9cbd5e54bca316e0885f8ffa
Jobbo <rjobling@gmail.com>
2026-04-25 08:00:33 -0400

    Add some different smaller music files

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ set PATH=%PATH%;%USERPROFILE%\.vscode\extensions\bartmanabyss.amiga-debug-1.7.7\
 @echo on
 
 cd data
-..\framework\tools\LSPConvert statetrue.mod -shrink -getpos -setpos
+..\framework\tools\LSPConvert cosmic_makeup.mod -shrink -getpos -setpos
 ..\framework\tools\abc2 font.png -bpc 2 -b font_bpls.bin
 ..\framework\tools\abc2 image.png -bpc 2 -uninterleaved -b image_bpls.bin
 ..\framework\tools\abc2 palettes.png -rgb -b palettes.bin
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd data
-../framework/tools/linux/LSPConvert statetrue.mod -shrink -getpos -setpos
+../framework/tools/linux/LSPConvert cosmic_makeup.mod -shrink -getpos -setpos
 ../framework/tools/linux/abc2 font.png -bpc 2 -b font_bpls.bin
 ../framework/tools/linux/abc2 image.png -bpc 2 -uninterleaved -b image_bpls.bin
 ../framework/tools/linux/abc2 palettes.png -rgb -b palettes.bin
