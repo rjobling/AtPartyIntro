4b9a1eab77f9bbb7ce0e795991c671bdc38fd2bf
Jobbo <rjobling@gmail.com>
2026-04-25 12:40:29 -0400

    Add logo to build

--- build.cmd
+++ build.cmd
@@ -7,6 +7,7 @@ set PATH=%PATH%;%USERPROFILE%\.vscode\extensions\bartmanabyss.amiga-debug-1.7.7\
 cd data
 ..\framework\tools\LSPConvert cosmic_makeup.mod -shrink -getpos -setpos
 ..\framework\tools\abc2 font.png -bpc 2 -b font_bpls.bin
+..\framework\tools\abc2 logo.png -bpc 4 -uninterleaved -b logo_bpls.bin -p logo_pal.bin
 ..\framework\tools\abc2 image.png -bpc 2 -uninterleaved -b image_bpls.bin
 ..\framework\tools\abc2 palettes.png -rgb -b palettes.bin
 cd ..
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 cd data
 ../framework/tools/linux/LSPConvert cosmic_makeup.mod -shrink -getpos -setpos
 ../framework/tools/linux/abc2 font.png -bpc 2 -b font_bpls.bin
+../framework/tools/abc2 logo.png -bpc 4 -uninterleaved -b logo_bpls.bin -p logo_pal.bin
 ../framework/tools/linux/abc2 image.png -bpc 2 -uninterleaved -b image_bpls.bin
 ../framework/tools/linux/abc2 palettes.png -rgb -b palettes.bin
 cd ..
